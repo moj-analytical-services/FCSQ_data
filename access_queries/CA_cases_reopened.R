3607c0af4189af7b136f8dbded7396e3ab881842
Leslie Afonso <les.afonso@justice.gov.uk>
2021-01-26 15:12:30 +0000

    children act case reopened query added

---
+++ access_queries/CA_cases_reopened.R
@@ -0,0 +1,24 @@
+#Query below runs the sql statement through Athena, using the dbtools function
+
+#Query is for children act case reopened events
+
+query_CA_reopened <- 
+ "SELECT  
+  E.CASE_NUMBER,
+  E.EVENT,
+  E.EVENT_MODEL,
+  E.RECEIPT_DATE,
+  E.ENTRY_DATE,
+  E.ERROR
+FROM 
+  familyman_dev_v2.EVENTS E
+WHERE 
+  E.EVENT_MODEL = 'G62'
+  AND E.ERROR = 'N'
+  AND E.mojap_snapshot_date = date '2020-10-30';"
+
+Table_CA_reopened <- dbtools::read_sql(query_CA_reopened)
+
+s3tools::write_df_to_csv_in_s3(Table_CA_reopened, "alpha-family-data/CSVs/CA_reopened_AP.csv")
+
+
