072e9f2eeb204f584c9a3c8afaa68801defdc9d6
jamie.cheke@justice.gov.uk <jamie.cheke@justice.gov.uk>
2022-08-26 14:39:30 +0000

    Updating FR to simplify code

--- tables/Accessible_Tables/table15_access.R
+++ tables/Accessible_Tables/table15_access.R
@@ -4,25 +4,11 @@
 
 ################################################################################
 
-t15_accessible_year <- fr_joined_year %>% 
-  filter(Year <= annual_year) %>% 
-  mutate(Qtr = NA) %>% 
-  relocate(Qtr, .after = Year)
+full_t15 <- bind_rows(t15_reg_year, t15_reg_qtr) %>% select(!starts_with("blank"))
 
-colnames(t15_accessible_year) <- c('Year', 'Quarter' ,'Uncontested Applications', 'Contested Applications',
+colnames(full_t15) <- c('Year', 'Quarter' ,'Uncontested Applications', 'Contested Applications',
                                    'Total Applications', 'Cases starting', 'Uncontested Disposals',
                                    'Initially Contested Disposals', 'Contested Disposals',
                                    'Total Disposals', 'Cases closed')
 
-
-t15_accessible_qtr <- fr_joined_qtr %>%
-  filter(Year >= 2009) %>% 
-  mutate(Qtr = paste0('Q', Qtr)) %>% 
-  relocate(Qtr, .after = Year)
-
-colnames(t15_accessible_qtr) <- c('Year', 'Quarter' ,'Uncontested Applications', 'Contested Applications',
-                                  'Total Applications', 'Cases starting', 'Uncontested Disposals',
-                                  'Initially Contested Disposals', 'Contested Disposals',
-                                  'Total Disposals', 'Cases closed')
-
-t15_accessible <- bind_rows(t15_accessible_year, t15_accessible_qtr)
+t15_accessible <- full_t15
