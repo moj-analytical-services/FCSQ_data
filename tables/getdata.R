b49c4bc84a8cae7eeae4b12cb2cf2841cdc9a84c
jamie.cheke@justice.gov.uk <jamie.cheke@justice.gov.uk>
2024-01-26 14:15:01 +0000

    Simplifying pub quarter

--- tables/getdata.R
+++ tables/getdata.R
@@ -22,7 +22,7 @@ source(paste0(path_to_project, "functions.R"))
 
 na_keys <- c(":", ".", "..", "*", "#REF!", "-", "**")
 
-child_act_csv <- read_using(readr::read_csv, paste0(csv_folder, "CSV Children Act", " ", pub_year, " Q", pub_quarter, ".csv" ), na = na_keys,
+child_act_csv <- read_using(readr::read_csv, paste0(csv_folder, "CSV Children Act", " ", pub_year_quarter, ".csv" ), na = na_keys,
                             col_types = cols(
                               YEAR = col_double(),
                               Qtr = col_character(),
@@ -39,55 +39,55 @@ child_act_csv <- read_using(readr::read_csv, paste0(csv_folder, "CSV Children Ac
                               HC_INDICATOR = col_character(),
                               Count = col_double())) %>% rename_with(str_to_title)
 
-divorce_csv <- read_using(readr::read_csv, paste0(csv_folder,"CSV Divorce National", " ", pub_year, " Q", pub_quarter, ".csv" ), na = na_keys) %>% 
+divorce_csv <- read_using(readr::read_csv, paste0(csv_folder,"CSV Divorce National", " ", pub_year_quarter, ".csv" ), na = na_keys) %>% 
   rename_with(str_to_title)
 
-fr_csv <- read_using(readr::read_csv, paste0(csv_folder,"CSV Financial Remedy National", " ", pub_year, " Q", pub_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
-dv_csv <- read_using(readr::read_csv, paste0(csv_folder, "CSV Domestic Violence National", " ", pub_year, " Q", pub_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
-adopt_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Adoptions National', " ", pub_year, " Q", pub_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
-fmpo_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV FMPOs National', " ", pub_year, " Q", pub_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
-fgm_csv <-  read_using(readr::read_csv, paste0(csv_folder, 'CSV FGMPOs National', " ", pub_year, " Q", pub_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
+fr_csv <- read_using(readr::read_csv, paste0(csv_folder,"CSV Financial Remedy National", " ", pub_year_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
+dv_csv <- read_using(readr::read_csv, paste0(csv_folder, "CSV Domestic Violence National", " ", pub_year_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
+adopt_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Adoptions National', " ", pub_year_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
+fmpo_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV FMPOs National', " ", pub_year_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
+fgm_csv <-  read_using(readr::read_csv, paste0(csv_folder, 'CSV FGMPOs National', " ", pub_year_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
 
-divorce_progress_csv <- read_using(readr::read_csv, paste0(csv_folder,'CSV Divorce Progression National', " ", pub_year, " Q", pub_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
-divorce_timeliness_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Divorce Timeliness', " ", pub_year, " Q", pub_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
+divorce_progress_csv <- read_using(readr::read_csv, paste0(csv_folder,'CSV Divorce Progression National', " ", pub_year_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
+divorce_timeliness_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Divorce Timeliness', " ", pub_year_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
 
-priv_law_disposal_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Timeliness Private Law Final Order', " ", pub_year, " Q", pub_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
+priv_law_disposal_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Timeliness Private Law Final Order', " ", pub_year_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
 
-probate_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Probate National', " ", pub_year, " Q", pub_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
-probate_timeliness_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Probate timeliness', " ", pub_year, " Q", pub_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
+probate_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Probate National', " ", pub_year_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
+probate_timeliness_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Probate timeliness', " ", pub_year_quarter, ".csv"), na = na_keys) %>% rename_with(str_to_title)
 
-opg_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV OPG Powers of Attorney National', " ", pub_year, " Q", pub_quarter, ".csv"), 
+opg_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV OPG Powers of Attorney National', " ", pub_year_quarter, ".csv"), 
                       na =  na_keys) %>% rename_with(str_to_title) %>% select(1:5)
 
-divorce_t12_input <- read_using(readr::read_csv, paste0(csv_folder, 'DIVORCE_FCSQ_T12', " ", pub_year, " Q", pub_quarter, '.csv'), na = na_keys) %>% rename_with(str_to_title)
-divorce_t12b_input <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Divorce New Law', " ", pub_year, " Q", pub_quarter, '.csv'), na = na_keys) %>% rename_with(str_to_title)
-dol_hc_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV High Court Deprivation of Liberty', " ", pub_year, " Q", pub_quarter, ' - Region level.csv'), na = na_keys) %>% rename_with(str_to_title)
+divorce_t12_input <- read_using(readr::read_csv, paste0(csv_folder, 'DIVORCE_FCSQ_T12', " ", pub_year_quarter, '.csv'), na = na_keys) %>% rename_with(str_to_title)
+divorce_t12b_input <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Divorce New Law', " ", pub_year_quarter, '.csv'), na = na_keys) %>% rename_with(str_to_title)
+dol_hc_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CSV High Court Deprivation of Liberty', " ", pub_year_quarter, ' - Region level.csv'), na = na_keys) %>% rename_with(str_to_title)
 
 #Additional non csv inputs
-care_disposal_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CARE_FINAL', " ", pub_year, " Q", pub_quarter,'.csv'), na = na_keys) %>% rename_with(str_to_title)
+care_disposal_csv <- read_using(readr::read_csv, paste0(csv_folder, 'CARE_FINAL', " ", pub_year_quarter,'.csv'), na = na_keys) %>% rename_with(str_to_title)
 
-table_3_lookup <- read_using(readr::read_csv, paste0(csv_folder, 'Table_3_lookup', " ", pub_year, " Q", pub_quarter,'.csv'), na = na_keys) %>% rename_with(str_to_title)
-table_10_lookup <- read_using(readr::read_csv, paste0(csv_folder, 'Table_10_lookup', " ", pub_year, " Q", pub_quarter,'.csv'), na = na_keys) %>% rename_with(str_to_title)
-table_11_lookup <- read_using(readr::read_csv, paste0(csv_folder, 'Table_11_lookup', " ", pub_year, " Q", pub_quarter,'.csv'), na = na_keys) %>% rename_with(str_to_title)
-table_10b_lookup <- read_using(readr::read_csv, paste0(csv_folder, 'Table_10b_lookup', " ", pub_year, " Q", pub_quarter,'.csv'), na = na_keys) %>% rename_with(str_to_title)
+table_3_lookup <- read_using(readr::read_csv, paste0(csv_folder, 'Table_3_lookup', " ", pub_year_quarter,'.csv'), na = na_keys) %>% rename_with(str_to_title)
+table_10_lookup <- read_using(readr::read_csv, paste0(csv_folder, 'Table_10_lookup', " ", pub_year_quarter,'.csv'), na = na_keys) %>% rename_with(str_to_title)
+table_11_lookup <- read_using(readr::read_csv, paste0(csv_folder, 'Table_11_lookup', " ", pub_year_quarter,'.csv'), na = na_keys) %>% rename_with(str_to_title)
+table_10b_lookup <- read_using(readr::read_csv, paste0(csv_folder, 'Table_10b_lookup', " ", pub_year_quarter,'.csv'), na = na_keys) %>% rename_with(str_to_title)
 
-cop_link <- paste0(csv_folder, "COP Tables ", pub_year, " Q", pub_quarter, ".xlsx")
+cop_link <- paste0(csv_folder, "COP Tables ", pub_year_quarter, ".xlsx")
 cop_table_21 <- read_using(openxlsx::read.xlsx, cop_link, sheet = "Table 21", check.names = TRUE, na.strings = na_keys)
 cop_table_22 <- read_using(openxlsx::read.xlsx, cop_link, sheet = "Table 22", check.names = TRUE, na.strings = na_keys)
 
-#cafcass <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Private law cases CAFCASS and MoJ', " ", pub_year, " Q", pub_quarter, ".csv") )%>% 
+#cafcass <- read_using(readr::read_csv, paste0(csv_folder, 'CSV Private law cases CAFCASS and MoJ', " ", pub_year_quarter, ".csv") )%>% 
   #rename_with(str_to_title) %>% select(1:5)
 
 # OPG Table. This is the spreadsheet given by the OPG team. Start row is the first row of data. The row under the column headings
-opg_table <- read_using(openxlsx::read.xlsx, paste0(csv_folder, 'OPG Tables', " ", pub_year, " Q", pub_quarter, ".xlsx"), sheet = "Table_23", 
+opg_table <- read_using(openxlsx::read.xlsx, paste0(csv_folder, 'OPG Tables', " ", pub_year_quarter, ".xlsx"), sheet = "Table_23", 
                         check.names = TRUE, na.strings = na_keys,  startRow = 9, skipEmptyRows = FALSE, skipEmptyCols = FALSE, colNames = FALSE)
 
 
-contest_probate <- read_using(readr::read_csv, paste0(csv_folder, 'Contested_Probate', " ", pub_year, " Q", pub_quarter,'.csv'), na = na_keys) %>% rename_with(str_to_title)
+contest_probate <- read_using(readr::read_csv, paste0(csv_folder, 'Contested_Probate', " ", pub_year_quarter,'.csv'), na = na_keys) %>% rename_with(str_to_title)
 
   
 #notes_import <- read_using(readr::read_csv, paste0(csv_folder, 'Notes.csv'), na = na_keys)
-notes_import <- read_using(openxlsx::read.xlsx, paste0(csv_folder, 'Notes Workbook', " ", pub_year, " Q", pub_quarter, '.xlsx'), sheet = 'Notes', na.strings = na_keys, sep.names = ' ') %>% as_tibble()
+notes_import <- read_using(openxlsx::read.xlsx, paste0(csv_folder, 'Notes Workbook', " ", pub_year_quarter, '.xlsx'), sheet = 'Notes', na.strings = na_keys, sep.names = ' ') %>% as_tibble()
 
 #notes_import <- openxlsx::read.xlsx(paste0(path_to_project, 'Notes Workbook 2022 Q2.xlsx'), sheet = 'Notes', na.strings = na_keys, sep.names = ' ') %>% as_tibble()
 
--- tables/variables.R
+++ tables/variables.R
@@ -6,7 +6,8 @@ annual_year <- 2022    # UPDATE IN THE FOURTH QUARTER OF EVERY YEAR
 pub_date <-"21st December 2023" # UPDATE EACH QUARTER
 next_pub_date <- "28th March 2024"  # UPDATE EACH QUARTER
 path_to_project = '~/FCSQ_data/tables/'                                       # UPDATE ONLY IF YOU CHANGE THE LOCATION OF THE PROJECT FILES
-csv_folder <- paste0("alpha-family-data/CSVs/CSV_bulletin/", pub_year, " Q",pub_quarter,"/") # location in the S3 bucket to import CSVs from
+pub_year_quarter <- paste0(pub_year, " Q", pub_quarter)
+csv_folder <- paste0("alpha-family-data/CSVs/CSV_bulletin/", pub_year_quarter, "/") # location in the S3 bucket to import CSVs from
 lookup_folder <- "alpha-family-data/CSVs/lookups/"
 
 # Numerical values that will be used for na and suppressed values. Numerical values must be used to keep numerical columns numerical
